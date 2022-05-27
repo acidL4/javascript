42a9f8a48321c29d9e2de6da509598e44ac35d11
Lautaro Sanz Orlando <sanz.lautaro@outlook.com>
2022-05-27 20:09:33 +0200

    Se agregan while y for

---
+++ main.js
@@ -0,0 +1,29 @@
+// Ejercicio WHILE
+
+let user = prompt('Esto es una calculadora. \n\nIngrese su usuario, si no quiere ingresar su usuario ingrese "salir"')
+
+while (user != 'continuar') {
+    
+    if (user == 'salir'){
+        break
+    }
+
+    console.log('Bienvenido ' + user);
+
+    user = prompt('Bienvenido ' + user + '. Escriba "continuar" para avanzar a la calculadora')
+}
+
+
+
+
+//Ejercicio FOR
+
+let multiplicando = parseInt(prompt('Que numero desea multiplicar? '))
+let multiplicador = parseInt(prompt('Cuantas veces desea multiplicarlo? '))
+
+for (let i = 1; i <= multiplicador ; i++) {
+    
+    producto = multiplicando * i;
+    console.log( multiplicando + 'x' + i + '=' + producto);
+    
+}
